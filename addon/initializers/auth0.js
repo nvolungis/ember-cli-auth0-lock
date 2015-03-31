321b04e6174d0f7150c8b9e7009cd718198bbfa3
Neil Volungis <neilvolungis@Neils-Air.local>
2015-03-31 12:58:59 -0400

    changed to my liking

--- addon/initializers/auth0.js
+++ addon/initializers/auth0.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
-
-var auth0 = Ember.Object.extend({
+var auth0 = Ember.Object.extend(Ember.Evented, {
   callbacks: {
-    onLoginSuccess:  function(){},
+    onLoginSuccess:  function(){
+      // window.location = 'activities';
+      console.log('login success');  
+    },
     onLoginError:    function(err){ console.log(err); },
     onSignupSuccess: function(){},
     onSignupError:   function(err){ console.log(err); }
@@ -11,17 +13,22 @@ var auth0 = Ember.Object.extend({
 
   // Redirect after use has been successfully logged out
   logoutUrl: null,
-  signupUrl: '/protected',
-  loginUrl:  '/login',
+  signupUrl: 'activities',
+  loginUrl:  'activities',
 
-  isAuthed: false,
+  isAuthed: localStorage.isAuthed == "true",
   isAnonymous: Ember.computed.not('isAuthed'),
 
   // Auth0 user profile
-  profile: null,
+  profile: localStorage.profile,
 
   // Auth0 access token
-  token: null,
+  token: localStorage.token,
+  userid: localStorage.userid,
+
+  getUser: function(){
+    JSON.parse(localStorage.user);
+  },
 
   // Auth0Lock configurable options
   // https://github.com/auth0/lock/wiki/Auth0Lock-customization
@@ -29,15 +36,14 @@ var auth0 = Ember.Object.extend({
     authParams: {}
   },
 
-  // update authentication properties
-  setAuth: function(token, profile) {
+  tokenChanged: function(){
+    localStorage.token = this.get('token');
+  }.observes('token'),
 
-    Ember.debug('Auth0-user successfully logged in');
+  isAuthedChanged: function(){
+    localStorage.isAuthed = this.get('isAuthed'); 
+  }.observes('isAuthed'),
 
-    this.set('token', token);
-    this.set('isAuthed', true);
-    this.set('profile', profile);
-  },
 
   // complete Lock widget, incl. signup and reset password option
   completeLogin: function() {
@@ -49,7 +55,8 @@ var auth0 = Ember.Object.extend({
         }
         this.setAuth(token, profile);
         this.callbacks.onLoginSuccess.call(this, token, profile);
-        window.location = this.loginUrl;
+        console.log('logged in', this);
+        if(this.loginUrl) window.location = this.loginUrl;
       }.bind(this)
     );
   },
@@ -63,7 +70,6 @@ var auth0 = Ember.Object.extend({
         }
         this.setAuth(token, profile);
         this.callbacks.onLoginSuccess.call(this, token, profile);
-        window.location = this.loginUrl;
       }.bind(this)
     );
   },
@@ -74,7 +80,7 @@ var auth0 = Ember.Object.extend({
         return this.callbacks.onSignupError.call(this, err);
       }
       this.callbacks.onSignupSuccess.call(this);
-      this.transitionTo(this.get('signupUrl'));
+      if(this.signupUrl) window.location = this.signupUrl;
     }.bind(this));
   },
 
@@ -82,45 +88,98 @@ var auth0 = Ember.Object.extend({
     this.authClient.logout({
       returnTo: this.get('logoutUrl')
     });
+  },
+
+  session_destroy: function(){
+    localStorage.userid = null;
+    localStorage.token = null;
+    localStorage.isAuthed = false;
+    this.set('userid', null);
+    this.set('user', null);
+    this.set('token', null);
+    this.set('isAuthed', false);
   }
 });
-
 
 export function initialize(container, application) {
   var config = container.lookupFactory('config:environment'),
       auth0Config = config ? config['ember-cli-auth0-lock'] : null;
 
   application.register('auth0:main', auth0, { singleton: true });
+  application.inject('adapter', 'auth0', 'auth0:main');
   application.inject('controller', 'auth0', 'auth0:main');
   application.inject('route', 'auth0', 'auth0:main');
-  application.inject('adapter', 'auth0', 'auth0:main');
+  application.inject('view', 'auth0', 'auth0:main');
 
   // add APP auth0 config options
   if (auth0Config) {
     auth0.reopen({
-
       logoutUrl: auth0Config.logoutUrl,
+      loginUrl: auth0Config.loginUrl,
 
       init: function() {
         var authParams = Ember.merge(this.get('lockOptions.authParams'), auth0Config.authParams);
 
         // update authparams config
         this.set('lockOptions.authParams', authParams);
-
         this.authClient = new Auth0Lock(auth0Config.cid, auth0Config.domain);
 
-        // insert dummy authentication data, useful for development
-        if (auth0Config.dummy) {
-          this.set('token', auth0Config.dummyToken);
-          this.set('isAuthed', true);
-          this.set('profile', auth0Config.dummyProfile);
+        if(this.userid){
+          this.syncUser();
         }
-      }
+      },
+
+      // update authentication properties
+      setAuth: function(token, profile) {
+        var self = this;
+        var store = container.lookup('store:main');
+        var user = store.createRecord('user', {
+          email: profile.email
+        });
+
+        console.log('set auth');
+
+        user.save().then(function(_user){
+          self.set('userid', _user.id);
+        });
+
+        this.set('token', token);
+        this.set('isAuthed', true);
+      },
+
+      clearAuth: function(){
+        this.set('token', null);
+        this.set('isAuthed', false);
+        this.set('user', null);
+        this.set('userid', null);
+      },
+
+      syncUser: function(){
+        var store = container.lookup('store:main'),
+            uid   = this.get('userid');
+
+        console.log('sync before', uid);
+
+        if(uid == null) return;
+
+        store.find('user', uid).then(function(user){
+          this.set('user', user);
+          console.log('user synced');
+          this.trigger('complete');
+        }.bind(this));
+
+        localStorage.userid = uid;
+      },
+
+      userChanged: function(){
+        this.syncUser();
+      }.observes('userid')
     });
   }
 }
 
 export default {
+  before: 'ember-data',
   name: 'auth0',
   initialize: initialize
 };
